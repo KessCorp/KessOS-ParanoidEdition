ccb9f09ae33d784c028449c96c71ce7c4393f20f
Ian-M-Moffett <ian@kesscoin.com>
2022-05-08 07:58:36 -0400

    Fixed mistake.

--- kernel/src/protection/asm/iopl.asm
+++ kernel/src/protection/asm/iopl.asm
@@ -7,8 +7,8 @@ global zero_iopl
 zero_iopl:
     pushf                   ;; Push FLAGS onto stack.
     pop rax                 ;; Pop FLAGS from stack into RAX.
-    and rax, ~(1 << 12)     ;; Zero bit 12 of FLAGS (bit 1 of IOPL).
-    and rax, ~(1 << 13)     ;; Zero bit 13 of FLAGS (bit 2 of IOPL).
+    and rax, ~(1 << 12)     ;; Zero bit 12 of FLAGS (bit 0 of IOPL).
+    and rax, ~(1 << 13)     ;; Zero bit 13 of FLAGS (bit 1 of IOPL).
     push rax                ;; Push RAX onto stack.
     popf                    ;; Pop FLAGS back in place.
     retq                    ;; Done, so return. 
