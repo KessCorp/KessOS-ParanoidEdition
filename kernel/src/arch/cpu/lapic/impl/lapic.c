c3603f35fbe31f7c3ba6ba13ff8115d5a019fa4f
Ian-M-Moffett <ian@kesscoin.com>
2022-05-08 22:49:18 -0400

    Got APIC base.

--- kernel/src/arch/cpu/lapic/impl/lapic.c
+++ kernel/src/arch/cpu/lapic/impl/lapic.c
@@ -8,6 +8,9 @@
 #include <stdint.h>
 
 
+static uint64_t apic_base = 0x0;
+
+
 static inline uint32_t lapic_supported() {
     uint32_t eax, unused, edx;
     __get_cpuid(1, &eax, &unused, &unused, &edx);
@@ -25,8 +28,19 @@ void lapic_init() {
         return;
     }
 
+    uint32_t lo;
+    uint32_t hi;
+
     log("Processor has an LAPIC.\n", S_INFO); 
     log("Disabling legacy PIC..\n", S_INFO);
     outportb(PIC1_DATA, 0xFF);
     outportb(PIC2_DATA, 0xFF);
+    log("Fetching APIC base from MSR 0x1B (bits 12-35)..\n", S_INFO); 
+    rdmsr(0x1B, &lo, &hi);
+
+    // Extract bits 12-35.
+    lo >>= 12;
+    apic_base = lo;
+    apic_base = (apic_base << 4) | (hi & 0xF);
+    log("APIC BASE: %x\n", S_INFO, apic_base);
 }
